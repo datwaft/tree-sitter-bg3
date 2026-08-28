556c826fe0c0f6b199d73f88ef99d329b3009c3d
David Guevara <datwaftx@gmail.com>
2026-08-28 10:04:52 -0600

    chore: prepare v0.7.0 (#44)

--- tree-sitter-bg3-thoth/src/parser.c
+++ tree-sitter-bg3-thoth/src/parser.c
@@ -14679,7 +14679,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_bg3_thoth(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 6,
+      .minor_version = 7,
       .patch_version = 0,
     },
   };
